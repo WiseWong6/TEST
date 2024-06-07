5c8bb0e3661f17371dbed8eec34b91797c15d1e0
Wise Wong <imhwc@icloud.com>
2024-06-07 22:19:01 +0800

    做作业之前的代码复习

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["🐶", "🐱", "🐭", "🐹", "🦊", "🐻", "🐼", "🐻‍❄️", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "🐋", "🐧", "🐦", "🐤", "🐺", "🐡"]
+    @State private var cardsCount = 4
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Memorize!").font(.largeTitle).bold()
+            ScrollView {
+                cards
+            }
+            Spacer()
+            buttonView
+        }.padding()
+    }
+
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120.0))], content: {
+            ForEach(0 ..< cardsCount, id: \.self) { index in
+                CardView(cardText: emojis[index]).aspectRatio(4 / 3, contentMode: .fill)
+            }
+        }).foregroundColor(.orange)
+    }
+
+    var buttonView: some View {
+        HStack {
+            cardAdder
+            Spacer()
+            cardRemover
         }
-        .padding()
+    }
+
+    var cardAdder: some View {
+        buttonAdjust(by: 1, buttonSymbol: "rectangle.stack.badge.plus.fill", buttonName: "添加卡片")
+    }
+
+    var cardRemover: some View {
+        buttonAdjust(by: -1, buttonSymbol: "rectangle.stack.badge.minus.fill", buttonName: "删除卡片")
+    }
+
+    func buttonAdjust(by offset: Int, buttonSymbol: String, buttonName: String) -> some View {
+        Button(action: {
+            cardsCount += offset
+            print("\(cardsCount)")
+        }, label: {
+            Image(systemName: buttonSymbol)
+            Text(buttonName).font(.title)
+        }).buttonStyle(.bordered).disabled(cardsCount + offset < 1 || cardsCount + offset > emojis.count)
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    var cardText: String
+    var body: some View {
+        ZStack {
+            let baseRect = RoundedRectangle(cornerRadius: 12.0)
+            if isFaceUp {
+                baseRect.foregroundStyle(.white)
+                baseRect.strokeBorder(lineWidth: 2.0)
+                Text(cardText).font(.largeTitle)
+            } else {
+                baseRect
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
